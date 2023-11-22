27e7238d0d6c51f52dc44d01822c49a06236d429
vannguyenv12 <npnv.vn1@gmail.com>
2023-11-23 02:55:20 +0700

    feat: Implement comment feature

---
+++ src/features/comments/controllers/add-comment.ts
@@ -0,0 +1,41 @@
+import { ICommentDocument, ICommentJob } from '@comment/interfaces/comment.interface';
+import { addCommentSchema } from '@comment/schemes/comment';
+import { joiValidation } from '@global/decorators/joi-validation.decorators';
+import { commentQueue } from '@service/queues/comment.queue';
+import { CommentCache } from '@service/redis/comment.cache';
+import { Request, Response } from 'express';
+import HTTP_STATUS from 'http-status-codes';
+import { ObjectId } from 'mongodb';
+
+const commentCache: CommentCache = new CommentCache();
+
+export class Add {
+  @joiValidation(addCommentSchema)
+  public async reaction(req: Request, res: Response): Promise<void> {
+    const { userTo, postId, profilePicture, comment } = req.body;
+    const commentObjectId: ObjectId = new ObjectId();
+    const commentData: ICommentDocument = {
+      _id: commentObjectId,
+      postId,
+      username: `${req.currentUser?.username}`,
+      avatarColor: `${req.currentUser?.avatarColor}`,
+      profilePicture,
+      comment,
+      createdAt: new Date()
+    } as ICommentDocument;
+
+    await commentCache.savePostCommentToCache(postId, JSON.stringify(commentData));
+
+    const databaseCommentData: ICommentJob = {
+      postId,
+      userTo,
+      userFrom: req.currentUser!.userId,
+      username: req.currentUser!.username,
+      comment: commentData
+    } as ICommentJob;
+
+    commentQueue.addCommentJob('addCommentToDB', databaseCommentData);
+
+    res.status(HTTP_STATUS.CREATED).json({ message: 'Comment added successfully!' });
+  }
+}
