26af9875a881852ede29027ecf249d1aca4dcf6b
vannguyenv12 <npnv.vn1@gmail.com>
2023-11-30 00:22:17 +0700

    feat: added user feature

--- src/shared/services/db/auth.service.ts
+++ src/shared/services/db/auth.service.ts
@@ -45,6 +45,24 @@ class AuthService {
     }).exec()) as IAuthDocument;
     return user;
   }
+
+  public async getAuthUserByUsername(username: string): Promise<IAuthDocument> {
+    const user: IAuthDocument = (await AuthModel.findOne({ username: Helpers.firstLetterUppercase(username) }).exec()) as IAuthDocument;
+    return user;
+  }
+
+  public async getAuthUserByEmail(email: string): Promise<IAuthDocument> {
+    const user: IAuthDocument = (await AuthModel.findOne({ email: Helpers.lowercase(email) }).exec()) as IAuthDocument;
+    return user;
+  }
+
+  public async getAuthUserByPasswordToken(token: string): Promise<IAuthDocument> {
+    const user: IAuthDocument = (await AuthModel.findOne({
+      passwordResetToken: token,
+      passwordResetExpires: { $gt: Date.now() }
+    }).exec()) as IAuthDocument;
+    return user;
+  }
 }
 
 export const authService: AuthService = new AuthService();
