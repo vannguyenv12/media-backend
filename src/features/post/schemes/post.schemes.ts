c30afc94fc441d1dac2fc0d87c8ed43525968ebe
vannguyenv12 <npnv.vn1@gmail.com>
2023-11-30 01:19:39 +0700

    feat: post video

--- src/features/post/schemes/post.schemes.ts
+++ src/features/post/schemes/post.schemes.ts
@@ -9,7 +9,9 @@ const postSchema: ObjectSchema = Joi.object().keys({
   profilePicture: Joi.string().optional().allow(null, ''),
   imgVersion: Joi.string().optional().allow(null, ''),
   imgId: Joi.string().optional().allow(null, ''),
-  image: Joi.string().optional().allow(null, '')
+  video: Joi.string().optional().allow(null, ''),
+  videoId: Joi.string().optional().allow(null, ''),
+  videoVersion: Joi.string().optional().allow(null, '')
 });
 
 const postWithImageSchema: ObjectSchema = Joi.object().keys({
@@ -24,7 +26,28 @@ const postWithImageSchema: ObjectSchema = Joi.object().keys({
   gifUrl: Joi.string().optional().allow(null, ''),
   profilePicture: Joi.string().optional().allow(null, ''),
   imgVersion: Joi.string().optional().allow(null, ''),
-  imgId: Joi.string().optional().allow(null, '')
+  imgId: Joi.string().optional().allow(null, ''),
+  video: Joi.string().optional().allow(null, ''),
+  videoId: Joi.string().optional().allow(null, ''),
+  videoVersion: Joi.string().optional().allow(null, '')
 });
 
-export { postSchema, postWithImageSchema };
+const postWithVideoSchema: ObjectSchema = Joi.object().keys({
+  video: Joi.string().required().messages({
+    'any.required': 'Video is a required field',
+    'string.empty': 'Video property is not allowed to be empty'
+  }),
+  post: Joi.string().optional().allow(null, ''),
+  bgColor: Joi.string().optional().allow(null, ''),
+  privacy: Joi.string().optional().allow(null, ''),
+  feelings: Joi.string().optional().allow(null, ''),
+  gifUrl: Joi.string().optional().allow(null, ''),
+  profilePicture: Joi.string().optional().allow(null, ''),
+  imgVersion: Joi.string().optional().allow(null, ''),
+  imgId: Joi.string().optional().allow(null, ''),
+  image: Joi.string().optional().allow(null, ''),
+  videoId: Joi.string().optional().allow(null, ''),
+  videoVersion: Joi.string().optional().allow(null, '')
+});
+
+export { postSchema, postWithImageSchema, postWithVideoSchema };
